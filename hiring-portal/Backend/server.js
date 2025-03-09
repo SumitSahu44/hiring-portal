ebbff4aa7dee86e0409fb335845e4f6e17855ac6
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-03-09 23:23:32 +0530

    Update server.js

--- hiring-portal/Backend/server.js
+++ hiring-portal/Backend/server.js
@@ -46,7 +46,7 @@ const options = { stats: true };
 compilex.init(options);
 
 
-
+// server running on Port or 5000 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
