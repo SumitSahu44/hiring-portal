721c01a7f2797b2ed55c3d2651781c0a058be1c5
Sumit Sahu <99080079+SumitSahu44@users.noreply.github.com>
2025-03-09 23:21:07 +0530

    Update companyRoutes.js

--- hiring-portal/Backend/routes/companyRoutes.js
+++ hiring-portal/Backend/routes/companyRoutes.js
@@ -2,6 +2,7 @@ const express=require('express');
 const router=express.Router();
 const companyController=require('../controllers/companyController');
 
+// all company specific routes here 
 router.post('/company',companyController.postCompany);
 router.get('/company',companyController.getCompany);
 router.get('/companies/:id',companyController.getCompanies);
